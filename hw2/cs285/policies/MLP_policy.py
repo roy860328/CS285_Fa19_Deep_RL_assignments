1aa867a2d9a42ae21c7c755b7d9df6fa4ecac274
roy <roy1997860328@gmail.com>
2020-03-01 17:14:47 +0800

    GAE test

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -234,7 +234,7 @@ class MLPPolicyPG(MLPPolicy):
             targets_n = (qvals - np.mean(qvals))/(np.std(qvals)+1e-8)
             # TODO: update the nn baseline with the targets_n
             # HINT1: run an op that you built in define_train_op
-            _, loss = self.sess.run([self.baseline_prediction, self.baseline_update_op, self.baseline_loss], feed_dict={self.observations_pl: observations, self.targets_n: targets_n})
+            _, _, loss = self.sess.run([self.baseline_prediction, self.baseline_update_op, self.baseline_loss], feed_dict={self.observations_pl: observations, self.targets_n: targets_n})
         return loss
 
 #####################################################
